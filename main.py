d3af005403f606a5a19f0df213d22a3b62e641d5
Marco Rogic <marco.rogic@itsrizzoli.it>
2024-06-14 16:57:24 +0200

    Starting setups

---
+++ main.py
@@ -0,0 +1,15 @@
+from bson import ObjectId
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://bruno:solo0182@rogic-cluster-free.tqzezwa.mongodb.net/?retryWrites=true&w=majority&appName=Rogic-cluster-free"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
